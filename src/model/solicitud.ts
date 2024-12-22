e36cbbd4b23ad096fd0f6d1da2150a0976f87c9f
luisjaviervd <luisjavadu@gmail.com>
2024-12-22 16:32:56 -0600

    Agregar 'Due Date' en detalle de solicitud y listado de solicitudes

--- src/model/solicitud.ts
+++ src/model/solicitud.ts
@@ -1,72 +1,70 @@
-import { EstatusPago } from "./estatus-pago";
-import { EstatusSolicitud } from "./estatus-solicitud";
-import { TipoSolicitud } from "./tipo-solicitud";
-
 export class Solicitud {
-      /*idSolicitud: number;
-      fechaInicio: Date;
-      cliente: string;
-      telefono: string;
-      email: string;
-      docusign: string;
-      amount: number;
-      abogado: string;
-      email_abogado: string;
-      firmaAbogados: string;
-      numeroDeCaso: string;
-      tipoSolicitud: TipoSolicitud;
-      estatusSolicitud: EstatusSolicitud;
-      estatusPago: EstatusPago;
-      usuarioRevisor: number;
-      usuarioRevisando: number;*/
-      idSolicitud: number;
-    fechaInicio: Date;
-    fechaNacimiento: Date;
-    adicional: string;
-    cliente: string;
-    telefono: string;
-    email: string;
-    docusign: string;
-    amount: number;
-    abogado: string;
-    email_abogado: string;
-    numeroDeCaso: string;
-    idTipoSolicitud: number;
-    idEstatusSolicitud: number;
-    idEstatusPago: number;
-    firmaAbogados: string;
-    tipoSolicitud: string;
-    estatusSolicitud: string;
-    estatusPago: string;
-    age: number;
-    fecha_schedule: Date;
-    idUsuarioRevisor: number;
-    importante: string;
-    usuarioRevisor: string;
-    usuarioRevisando: string;
-    idUsuarioRevisando: number;
+  /*idSolicitud: number;
+  fechaInicio: Date;
+  cliente: string;
+  telefono: string;
+  email: string;
+  docusign: string;
+  amount: number;
+  abogado: string;
+  email_abogado: string;
+  firmaAbogados: string;
+  numeroDeCaso: string;
+  tipoSolicitud: TipoSolicitud;
+  estatusSolicitud: EstatusSolicitud;
+  estatusPago: EstatusPago;
+  usuarioRevisor: number;
+  usuarioRevisando: number;*/
+  idSolicitud: number;
+  fechaInicio: Date;
+  fechaNacimiento: Date;
+  adicional: string;
+  cliente: string;
+  telefono: string;
+  email: string;
+  docusign: string;
+  amount: number;
+  abogado: string;
+  email_abogado: string;
+  numeroDeCaso: string;
+  idTipoSolicitud: number;
+  idEstatusSolicitud: number;
+  idEstatusPago: number;
+  firmaAbogados: string;
+  tipoSolicitud: string;
+  estatusSolicitud: string;
+  estatusPago: string;
+  age: number;
+  fecha_schedule: Date;
+  idUsuarioRevisor: number;
+  importante: string;
+  usuarioRevisor: string;
+  usuarioRevisando: string;
+  idUsuarioRevisando: number;
 
-    idioma: string;
-    tipoEntrevista: string;
-    direccion: string;
-    estado: string;
-    referencia: string;
+  idioma: string;
+  tipoEntrevista: string;
+  direccion: string;
+  estado: string;
+  referencia: string;
 
-    apellidos: string;
+  apellidos: string;
 
-    usuarioRevisandoNombre: string;
-    asignacionTemplate: boolean;
+  usuarioRevisandoNombre: string;
+  asignacionTemplate: boolean;
 
-    waiver: boolean;
-    paralegalName: string;
-    paralegalEmails: string;
-    paralegalTelefonos: string;
+  waiver: boolean;
+  paralegalName: string;
+  paralegalEmails: string;
+  paralegalTelefonos: string;
 
-    asignacionIntSc: boolean;
-    usuarioIntScNombre: string;
+  asignacionIntSc: boolean;
+  usuarioIntScNombre: string;
 
-    fecha_schedule_scales: Date;
+  fecha_schedule_scales: Date;
 
-    external: boolean;
-    usuarioExternal: number;
+  external: boolean;
+  usuarioExternal: number;
+
+  dueDate: Date;
 }
