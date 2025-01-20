14782ed8c1417418e1f1dbb8fb08c3b38a2584ad
luisjaviervd <luisjavadu@gmail.com>
2025-01-19 19:42:37 -0600

    Cambio de formato de fecha a mm/dd/yyyy

--- src/app/common/pipes/date-pipe.component.ts
+++ src/app/common/pipes/date-pipe.component.ts
@@ -1,10 +1,11 @@
 import { Pipe } from "@angular/core";
 
 @Pipe({
-	name: "dateYYYYMMDD"
+	name: "dateMMDDYYYY"
 })
 export class DatePipe {
 	transform(value: string): string {
-    return value ? value.split("T")[0].replace(/-/g, '/') : "";
+    let aux = (value.split("T")[0]).split("-");
+    return aux.length == 3 ? aux[1] + "/" + aux[2] + "/" + aux[0] : "";
   }
 }
