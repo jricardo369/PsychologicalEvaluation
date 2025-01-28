7f631570fb9e61108cfa4d65d91771dfb482c1ce
luisjaviervd <luisjavadu@gmail.com>
2025-01-27 18:32:03 -0600

    correccion formato fecha en 'Files' contemplando horas

--- src/app/common/pipes/date-pipe.component.ts
+++ src/app/common/pipes/date-pipe.component.ts
@@ -4,10 +4,11 @@ import { Pipe } from "@angular/core";
 	name: "dateMMDDYYYY"
 })
 export class DatePipe {
-	transform(value: string): string {
+	transform(value: string, caseType: string = 'date'): string {
     if(value !== null && value.length > 0) {
-      let aux = (value.split("T")[0]).split("-");
-      return aux.length == 3 ? aux[1] + "/" + aux[2] + "/" + aux[0] : "";
+      let date = value.split(/[\sT]/);
+      let aux = (date[0].split("T")[0]).split("-");
+      return (aux.length == 3 ? aux[1] + "/" + aux[2] + "/" + aux[0] : "") + ((date.length > 1 && caseType == 'time')? " " + date[1] : "");
     }
     return;
   }
